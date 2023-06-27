8c855bab27e1c2d1ce94226604099c62bc86abd7
MaximilianoMO95 <131606838+MaximilianoMO95@users.noreply.github.com>
2023-06-27 01:42:56 -0400

    Update HomeView.java

--- demo/app/src/main/java/demo/views/HomeView.java
+++ demo/app/src/main/java/demo/views/HomeView.java
@@ -7,7 +7,7 @@ public class HomeView extends JPanel {
         public HomeView() {
                 setLayout(new BorderLayout());
 
-                // Create a JLabel for the app logo
+               
                 JLabel logoLabel = new JLabel();
                 ImageIcon logoIcon = new ImageIcon("src/main/java/demo/assets/favicon.png");
                 logoLabel.setIcon(logoIcon);
@@ -15,7 +15,7 @@ public class HomeView extends JPanel {
                 logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
                 logoLabel.setVerticalAlignment(SwingConstants.CENTER);
 
-                // Add the logo label to the center of the panel
+                
                 add(logoLabel, BorderLayout.CENTER);
         }
 }
