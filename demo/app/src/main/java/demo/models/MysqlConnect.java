e56c14b0f5dc20c3a248b1ae42bc33a49fb338be
mr-ema <ema.a.v.c2017@gmail.com>
2023-07-06 22:21:05 -0400

    feat: add mysql connector model

--- demo/app/src/main/java/demo/models/MysqlConnect.java
+++ demo/app/src/main/java/demo/models/MysqlConnect.java
@@ -7,31 +7,44 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MysqlConnect {
+        private static final String ENVDIR = "src/main/java/demo/assets";
         private final String DB_URL;
         private final String DB_USER;
         private final String DB_PASSWORD;
 
         private Connection connection;
 
+        // Default
         public MysqlConnect() {
                 Dotenv dotenv = Dotenv.configure()
-                        .directory("assets/config.env")
-                        .ignoreIfMalformed()
-                        .ignoreIfMissing()
+                        .directory(ENVDIR)
+                        .filename("config.env")
                         .load();
 
                 DB_URL = dotenv.get("DB_URL");
-                DB_USER = dotenv.get("DB_USER");
+                DB_USER = dotenv.get("DB_USERNAME");
                 DB_PASSWORD = dotenv.get("DB_PASSWORD");
         }
 
-        public Connection connect() {
+        public MysqlConnect(String fileName) {
+                Dotenv dotenv = Dotenv.configure()
+                        .directory(ENVDIR)
+                        .filename(fileName)
+                        .load();
+
+                DB_URL = dotenv.get("DB_URL");
+                DB_USER = dotenv.get("DB_USERNAME");
+                DB_PASSWORD = dotenv.get("DB_PASSWORD");
+        }
+
+        public Connection connect() throws SQLException {
                 // Create new connection
                 if (connection == null) {
                         try {
                                 connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
-                        } catch (SQLException e) { e.printStackTrace(); }
+                        // Fix this dumb catch-rethrow
+                        } catch (SQLException e) { throw e; }
                 }
 
                 return connection;
