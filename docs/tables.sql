f744034cbf1ea99914ce8d7f9ef0ce56cd786979
mr-ema <ema.a.v.c2017@gmail.com>
2023-07-09 19:14:11 -0400

    another desnormalization

--- docs/tables.sql
+++ docs/tables.sql
@@ -8,16 +8,10 @@ CREATE TABLE IF NOT EXISTS cliente (
     ap_paterno          VARCHAR(30) NOT NULL,
     tel                 VARCHAR(16) NOT NULL,
     comuna              VARCHAR(30) NOT NULL,
-    domicilio           VARCHAR(60) NOT NULL
-);
+    domicilio           VARCHAR(60) NOT NULL,
 
-CREATE TABLE IF NOT EXISTS cuenta (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-
-    cliente_id INT NOT NULL,
-    numero INT NOT NULL,
-    saldo INT NOT NULL,
-    descripcion VARCHAR(40) NOT NULL,
-
-    CONSTRAINT fk_cuenta_cliente FOREIGN KEY (cliente_id) REFERENCES cliente(id)
+    /* Cuenta */
+    numero_cuenta       INT NOT NULL,
+    saldo_cuenta        INT NOT NULL,
+    tipo_cuenta         VARCHAR(40) NOT NULL,
 );
